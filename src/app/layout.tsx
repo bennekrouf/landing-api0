bc4a7b0d0003b040b8f8efec32a2a0f921665296
Bennekrouf <mohamed.bennekrouf@gmail.com>
2025-03-02 22:13:27 +0100

    Fix eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,39 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+//
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Mayo",
   description: "AI Bridge to Enterprise systems",
   icons: {
-      icon: [
-        { url: '/favicon/favicon.jpg' },
-        { url: '/favicon/icon.png' },
-      ],
-      apple: [
-        { url: '/favicon/apple-icon.png' },
-      ],
-      other: [
-        {
-          rel: 'apple-touch-icon',
-          url: '/favicon/apple-icon.png',
-        },
-        {
-          rel: 'manifest',
-          url: '/favicon/site.webmanifest',
-        }
-      ],
-    },
+    icon: [
+      { url: '/favicon/favicon.jpg' },
+      { url: '/favicon/icon.png' },
+    ],
+    apple: [
+      { url: '/favicon/apple-icon.png' },
+    ],
+    other: [
+      {
+        rel: 'apple-touch-icon',
+        url: '/favicon/apple-icon.png',
+      },
+      {
+        rel: 'manifest',
+        url: '/favicon/site.webmanifest',
+      }
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -43,9 +43,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-    <head>
-    <meta name="apple-mobile-web-app-title" content="Mayo" />
-    </head>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Mayo" />
+      </head>
       <body className="bg-gray-900 antialiased">{children}</body>
     </html>
   )
